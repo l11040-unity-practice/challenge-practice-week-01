89309d19ceff66e0a07dcffd46aef19d6dc776fd
l11040 <113540683+l11040@users.noreply.github.com>
2024-05-13 09:35:31 +0900

    Player Input 구성 및 스페이스바 적용

--- Assets/Scripts/Challenge/RocketControllerC.cs
+++ Assets/Scripts/Challenge/RocketControllerC.cs
@@ -5,10 +5,10 @@ public class RocketControllerC : MonoBehaviour
 {
     private EnergySystemC _energySystem;
     private RocketMovementC _rocketMovement;
-    
+
     private bool _isMoving;
     private float _movementDirection;
-    
+
     private readonly float ENERGY_TURN = 0.5f;
     private readonly float ENERGY_BURST = 2f;
 
@@ -17,13 +17,13 @@ public class RocketControllerC : MonoBehaviour
         _energySystem = GetComponent<EnergySystemC>();
         _rocketMovement = GetComponent<RocketMovementC>();
     }
-    
+
     private void FixedUpdate()
     {
         if (!_isMoving) return;
-        
-        if(!_energySystem.UseEnergy(Time.fixedDeltaTime * ENERGY_TURN)) return;
-        
+
+        if (!_energySystem.UseEnergy(Time.fixedDeltaTime * ENERGY_TURN)) return;
+
         _rocketMovement.ApplyMovement(_movementDirection);
     }
 
@@ -33,4 +33,8 @@ public class RocketControllerC : MonoBehaviour
 
     // OnBoost 구현
     // private void OnBoost...
+    private void OnBoost(InputValue value)
+    {
+        Debug.Log("탭");
+    }
 }
